f974743065dcda61f755d81056a5f387ef1896a0
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-03-13 20:31:50 +0100

    Return to current directory after each install

--- .github/workflows/scripts/macos.bash
+++ .github/workflows/scripts/macos.bash
@@ -3,10 +3,9 @@ brew install bash
 brew upgrade cmake
 brew install unzip
 
-
-
 export CC=/usr/local/bin/gcc-13
 export CXX=/usr/local/bin/g++-13
+export FC=/usr/local/bin/gfortran-13
 export MACOSX_DEPLOYMENT_TARGET=11.0
 wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
 tar xvf gmp-6.3.0.tar.xz
@@ -14,11 +13,7 @@ cd gmp-6.3.0
 ./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
 make install -j
 
-
-
-export CC=/usr/local/bin/gcc-13
-export CXX=/usr/local/bin/g++-13
-export MACOSX_DEPLOYMENT_TARGET=11.0
+cd $GITHUB_WORKSPACE
 wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
 tar -xvf v5.1.1-DistDGL-v0.5.tar.gz
 wget https://github.com/KarypisLab/GKlib/archive/refs/tags/METIS-v5.1.1-DistDGL-0.5.tar.gz
@@ -35,18 +30,17 @@ make config prefix=$GITHUB_WORKSPACE/metis/ gklib_path=$GITHUB_WORKSPACE/GKlib-M
 make
 make install
 
-    export CC=/usr/local/bin/gcc-13
-    export CXX=/usr/local/bin/g++-13
-    export FC=/usr/local/bin/gfortran-13
-    export MACOSX_DEPLOYMENT_TARGET=11.0
-    git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
-    cd ThirdParty-Mumps
-    ./get.Mumps
-    ./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install
-    make
-    make install
+
+cd $GITHUB_WORKSPACE
+git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
+cd ThirdParty-Mumps
+./get.Mumps
+./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install
+make
+make install
 
 
+cd $GITHUB_WORKSPACE
 wget https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.12.zip
 unzip 3.14.12.zip
 echo 'enable_shared=no
@@ -57,10 +51,6 @@ with_metis_cflags="-I$GITHUB_WORKSPACE/metis/include"
 with_metis_lflags="-L$GITHUB_WORKSPACE/metis/lib -lmetis"' > $GITHUB_WORKSPACE/scip_install/share/config.site
 
 
-export CC=/usr/local/bin/gcc-13
-export CXX=/usr/local/bin/g++-13
-export FC=/usr/local/bin/gfortran-13
-export MACOSX_DEPLOYMENT_TARGET=11.0
 cd Ipopt-releases-3.14.12
 mkdir build
 cd build
@@ -70,10 +60,7 @@ make test
 make install
 
 
-export CC=/usr/local/bin/gcc-13
-export CXX=/usr/local/bin/g++-13
-export FC=/usr/local/bin/gfortran-13
-export MACOSX_DEPLOYMENT_TARGET=11.0
+cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/soplex/archive/refs/tags/release-700.zip
 unzip release-700.zip
 cd soplex-release-700
@@ -85,10 +72,7 @@ make test
 make install
 
 
-export CC=/usr/local/bin/gcc-13
-export CXX=/usr/local/bin/g++-13
-export FC=/usr/local/bin/gfortran-13
-export MACOSX_DEPLOYMENT_TARGET=11.0
+cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/scip/archive/refs/tags/v900.zip
 unzip v900.zip
 cd scip-900
