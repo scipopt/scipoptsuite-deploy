f5d2819784d3c8bc5bc9bcaa007bc7db772d8e74
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-03-15 11:59:14 +0100

    Fix macos arm script cd's

--- .github/workflows/scripts/macos_arm.bash
+++ .github/workflows/scripts/macos_arm.bash
@@ -6,50 +6,53 @@ echo 'enable_shared=no
 enable_java=no
 enable_sipopt=no
 with_pic=yes
-with_metis_cflags="-I$GITHUB_WORKSPACE/pyscipopt-install/metis/include"
-with_metis_lflags="-L$GITHUB_WORKSPACE/pyscipopt-install/metis/lib -lmetis"' > scip_install/share/config.site
+with_metis_cflags="-I$GITHUB_WORKSPACE/metis/include"
+with_metis_lflags="-L$GITHUB_WORKSPACE/metis/lib -lmetis"' > scip_install/share/config.site
 
 wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
 tar xvf gmp-6.3.0.tar.xz
 cd gmp-6.3.0
-./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/pyscipopt-install/scip_install
+./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
 make install -j
-cd ..
 
+
+cd $GITHUB_WORKSPACE
 wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
 tar -xvf v5.1.1-DistDGL-v0.5.tar.gz
 wget https://github.com/KarypisLab/GKlib/archive/refs/tags/METIS-v5.1.1-DistDGL-0.5.tar.gz
 tar -xvf METIS-v5.1.1-DistDGL-0.5.tar.gz
 mkdir metis
 cd GKlib-METIS-v5.1.1-DistDGL-0.5
-make config prefix=$GITHUB_WORKSPACE/pyscipopt-install/GKlib-METIS-v5.1.1-DistDGL-0.5
+make config prefix=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
 make
 make install
 sed -i'' -e 's/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Werror -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/g' GKlibSystem.cmake
-cd ..
+
+cd $GITHUB_WORKSPACE
 cd METIS-5.1.1-DistDGL-v0.5
-make config prefix=$GITHUB_WORKSPACE/pyscipopt-install/metis/ gklib_path=$GITHUB_WORKSPACE/pyscipopt-install/GKlib-METIS-v5.1.1-DistDGL-0.5
+make config prefix=$GITHUB_WORKSPACE/metis/ gklib_path=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
 make
 make install
-cd ..
 
+cd $GITHUB_WORKSPACE
 git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps
 ./get.Mumps
-./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/pyscipopt-install/scip_install
+./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install
 make
 make install
-cd ..
 
+
+cd $GITHUB_WORKSPACE
 cd Ipopt-releases-$IPOPT_VERSION
 mkdir build
 cd build
-../configure --prefix=$GITHUB_WORKSPACE/pyscipopt-install/scip_install/
+../configure --prefix=$GITHUB_WORKSPACE/scip_install/
 make -j$(nproc)
 make test
 make install
-cd ../..
 
+cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
 unzip release-$SOPLEX_VERSION.zip
 cd soplex-release-$SOPLEX_VERSION
@@ -59,8 +62,8 @@ cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -D
 make -j$(nproc)
 make test
 make install
-cd ../..
 
+cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
 unzip v$SCIP_VERSION.zip
 cd scip-$SCIP_VERSION
@@ -72,5 +75,6 @@ make install
 cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DSHARED=false
 make -j$(nproc)
 make install
-cd ../..
+
+cd $GITHUB_WORKSPACE
 zip -r $GITHUB_WORKSPACE/libscip-macos-arm.zip scip_install/lib scip_install/include scip_install/bin
