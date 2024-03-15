98551354b72f7de96f4e625e4f4965c06afbaec3
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-03-15 08:47:31 +0100

    Use relative paths to github workspace in linux workflow

--- .github/workflows/scripts/linux.bash
+++ .github/workflows/scripts/linux.bash
@@ -1,4 +1,4 @@
-cd /
+cd $GITHUB_WORKSPACE
 yum install gcc gcc-c++ libgfortran git patch wget lapack-static unzip zip make glibc-static -y
 rm -f /usr/lib64/liblapack.s*
 rm -f /usr/lib64/libblas.*
@@ -9,13 +9,12 @@ cd build
 cmake ..
 make -j
 mv lib/libblas.a /usr/lib64/.
-cd ..
-cd ..
-mkdir scip_install
-wget https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.12.zip
-unzip 3.14.12.zip
+
+cd $GITHUB_WORKSPACE
 mkdir scip_install
 mkdir scip_install/share
+wget https://github.com/coin-or/Ipopt/archive/refs/tags/releases/3.14.12.zip
+unzip 3.14.12.zip
 echo 'enable_shared=no
 enable_java=no
 enable_sipopt=no
@@ -24,40 +23,45 @@ with_metis_cflags="-I/metis/include/"
 with_metis_lflags="-L/metis/lib -lmetis -lm"
 with_lapack_lflags="-llapack_pic -lblas -lgfortran -lquadmath -lm"
 LT_LDFLAGS=-all-static
-LDFLAGS=-static' > /scip_install/share/config.site
+LDFLAGS=-static' > $GITHUB_WORKSPACE/scip_install/share/config.site
+
 wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
 tar xvf gmp-6.3.0.tar.xz
 cd gmp-6.3.0
-./configure --with-pic --disable-shared --enable-cxx --prefix=/scip_install
+./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
 make install -j
-cd ..
-rm scip_install/lib/*.so*
+
+cd $GITHUB_WORKSPACE
+rm $GITHUB_WORKSPACE/scip_install/lib/*.so*
 wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
 tar -xvf v5.1.1-DistDGL-v0.5.tar.gz
 wget https://github.com/KarypisLab/GKlib/archive/refs/tags/METIS-v5.1.1-DistDGL-0.5.tar.gz
 tar -xvf METIS-v5.1.1-DistDGL-0.5.tar.gz
-mkdir /metis
+mkdir $GITHUB_WORKSPACE/metis
 cd GKlib-METIS-v5.1.1-DistDGL-0.5
-make config prefix=/GKlib-METIS-v5.1.1-DistDGL-0.5
+make config prefix=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
 make
 make install
-cd ..
+
+cd $GITHUB_WORKSPACE
 cd METIS-5.1.1-DistDGL-v0.5
-make config prefix=/metis/ gklib_path=/GKlib-METIS-v5.1.1-DistDGL-0.5
+make config prefix=$GITHUB_WORKSPACE/metis/ gklib_path=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
 make
 make install
-cd ..
+
+cd $GITHUB_WORKSPACE
 git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps
 ./get.Mumps
-./configure --enable-shared=no --enable-static=yes --prefix=/scip_install
+./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install
 make -j
 make install
-cd ..
+
+cd $GITHUB_WORKSPACE
 cd Ipopt-releases-3.14.12
 mkdir build
 cd build
-../configure --prefix=/scip_install/
+../configure --prefix=$GITHUB_WORKSPACE/scip_install/
 make -j$(nproc)
 make test V=1 || :
 make install
@@ -68,23 +72,24 @@ unzip release-700.zip
 cd soplex-release-700
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=../../scip_install -DWITH_SHARED_LIBS=off
+cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DWITH_SHARED_LIBS=off
 make -j$(nproc)
 make test
 make install
-cd ../..
+
+cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/scip/archive/refs/tags/v900.zip
 unzip v900.zip
 cd scip-900
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSOPLEX_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DGMP_DIR=../../scip_install
+cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSOPLEX_DIR=$GITHUB_WORKSPACE/scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=$GITHUB_WORKSPACE/scip_install -DGMP_DIR=$GITHUB_WORKSPACE/scip_install
 make -j$(nproc) VERBOSE=true
 make install
-cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSOPLEX_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DGMP_DIR=../../scip_install -DSHARED=false
+cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSOPLEX_DIR=$GITHUB_WORKSPACE/scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=$GITHUB_WORKSPACE/scip_install -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DSHARED=false
 make -j$(nproc) VERBOSE=true
 make install
-cd ../..
+cd $GITHUB_WORKSPACE
 mkdir scip_install/lib
 mv scip_install/lib64/* scip_install/lib/.
 zip -r $GITHUB_WORKSPACE/libscip-linux.zip scip_install/lib scip_install/include scip_install/bin
