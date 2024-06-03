45d9f2c51f6451e422b6995995967a424d90c94b
Mark Turner <turner@zib.de>
2024-06-03 13:02:09 +0200

    Add MACOS flag to macos-arm

--- .github/workflows/scripts/macos_arm.bash
+++ .github/workflows/scripts/macos_arm.bash
@@ -1,6 +1,7 @@
 rm -rf /usr/local/include/boost
 mkdir /usr/local/include/boost
 brew install boost
+export MACOSX_DEPLOYMENT_TARGET=11.0
 
 wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
 tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
