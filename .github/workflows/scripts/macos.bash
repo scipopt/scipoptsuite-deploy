549bed14e1de214422362d72465a931f4cfcbb05
Mark Turner <turner@zib.de>
2024-06-04 16:31:27 +0200

    Add metis to global install. Remove comment out SOPLEX

--- .github/workflows/scripts/macos.bash
+++ .github/workflows/scripts/macos.bash
@@ -15,12 +15,12 @@ wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82
 tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
 mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
 
-#rm -f /usr/local/lib/libgmp*
-#wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
-#tar xvf gmp-6.3.0.tar.xz
-#cd gmp-6.3.0
-#./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
-#make install -j
+rm -f /usr/local/lib/libgmp*
+wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
+tar xvf gmp-6.3.0.tar.xz
+cd gmp-6.3.0
+./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
+make install -j
 
 #cd $GITHUB_WORKSPACE
 #wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
@@ -40,7 +40,6 @@ mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
 #make install
 mkdir $GITHUB_WORKSPACE/metis
 cd $GITHUB_WORKSPACE/metis-5.1.0
-sed -i '' '3i\ 3 cmake_policy(SET CMP0042 NEW)\ ' CMakeLists.txt
 make config shared=0 prefix=$GITHUB_WORKSPACE/metis/
 make
 make install
@@ -50,7 +49,12 @@ cd $GITHUB_WORKSPACE
 git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps
 ./get.Mumps
-./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install
+./configure --enable-shared=no --enable-static=yes --prefix=/$GITHUB_WORKSPACE/scip_install
+make
+make install
+# Just install this stuff globally to check if MUMPS finds it
+make clean
+./configure --enable-shared=no --enable-static=yes
 make
 make install
 
@@ -75,27 +79,27 @@ make test
 make install
 
 
-#cd $GITHUB_WORKSPACE
-#wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
-#unzip release-$SOPLEX_VERSION.zip
-#cd soplex-release-$SOPLEX_VERSION
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
-#make -j
-#make test
-#make install
-#
-#
-#cd $GITHUB_WORKSPACE
-#wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
-#unzip v$SCIP_VERSION.zip
-#cd scip-$SCIP_VERSION
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true
-#make -j
-#make test
-#make install
-#cd ../..
-#zip -r $GITHUB_WORKSPACE/libscip-macos.zip scip_install/lib scip_install/include scip_install/bin
+cd $GITHUB_WORKSPACE
+wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
+unzip release-$SOPLEX_VERSION.zip
+cd soplex-release-$SOPLEX_VERSION
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
+make -j
+make test
+make install
+
+
+cd $GITHUB_WORKSPACE
+wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
+unzip v$SCIP_VERSION.zip
+cd scip-$SCIP_VERSION
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true
+make -j
+make test
+make install
+cd ../..
+zip -r $GITHUB_WORKSPACE/libscip-macos.zip scip_install/lib scip_install/include scip_install/bin
