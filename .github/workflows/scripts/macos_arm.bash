a47b6b5a4edf1c8ad6f2f1f6f1a91f8dd975d9c2
Mark Turner <turner@zib.de>
2024-06-05 17:33:58 +0200

    Make intel all

--- .github/workflows/scripts/macos_arm.bash
+++ .github/workflows/scripts/macos_arm.bash
@@ -2,14 +2,11 @@ rm -rf /usr/local/include/boost
 mkdir /usr/local/include/boost
 brew install boost
 export MACOSX_DEPLOYMENT_TARGET=11.0
-export FC=/opt/homebrew/bin/gfortran-13
-export CC=/opt/homebrew/bin/gcc-13
-export CXX=/opt/homebrew/bin/g++-13
 export DEVELOPER_DIR=/Applications/Xcode_14.3.1.app/Contents/Developer
 
-#wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
-#tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
-#mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
+wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
+tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
+mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
 
 wget https://github.com/coin-or/Ipopt/archive/refs/tags/releases/$IPOPT_VERSION.zip
 unzip $IPOPT_VERSION.zip
@@ -22,12 +19,12 @@ with_pic=yes
 with_metis_cflags=\"-I${GITHUB_WORKSPACE}/metis/include\"
 with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > scip_install/share/config.site
 
-#rm -f /usr/local/lib/libgmp*
-#wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
-#tar xvf gmp-6.3.0.tar.xz
-#cd gmp-6.3.0
-#./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
-#make install -j
+rm -f /usr/local/lib/libgmp*
+wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
+tar xvf gmp-6.3.0.tar.xz
+cd gmp-6.3.0
+./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
+make install -j
 
 
 #cd $GITHUB_WORKSPACE
@@ -49,7 +46,7 @@ with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > scip_install/sh
 cd $GITHUB_WORKSPACE
 mkdir $GITHUB_WORKSPACE/metis
 cd $GITHUB_WORKSPACE/metis-5.1.0
-make config shared=0 prefix=$GITHUB_WORKSPACE/metis/ cc=gcc
+make config shared=0 prefix=$GITHUB_WORKSPACE/metis/
 make
 make install
 
@@ -72,27 +69,27 @@ make -j$(nproc)
 make test
 make install
 
-#cd $GITHUB_WORKSPACE
-#wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
-#unzip release-$SOPLEX_VERSION.zip
-#cd soplex-release-$SOPLEX_VERSION
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=../../scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
-#make -j$(nproc)
-#make test
-#make install
-#
-#cd $GITHUB_WORKSPACE
-#wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
-#unzip v$SCIP_VERSION.zip
-#cd scip-$SCIP_VERSION
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true -DGMP=true -DGMP_DIR=$GITHUB_WORKSPACE/scip_install
-#make -j
-#make test
-#make install
-#
-#cd $GITHUB_WORKSPACE
-#zip -r $GITHUB_WORKSPACE/libscip-macos-arm.zip scip_install/lib scip_install/include scip_install/bin
+cd $GITHUB_WORKSPACE
+wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
+unzip release-$SOPLEX_VERSION.zip
+cd soplex-release-$SOPLEX_VERSION
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=../../scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
+make -j$(nproc)
+make test
+make install
+
+cd $GITHUB_WORKSPACE
+wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
+unzip v$SCIP_VERSION.zip
+cd scip-$SCIP_VERSION
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true -DGMP=true -DGMP_DIR=$GITHUB_WORKSPACE/scip_install
+make -j
+make test
+make install
+
+cd $GITHUB_WORKSPACE
+zip -r $GITHUB_WORKSPACE/libscip-macos-arm.zip scip_install/lib scip_install/include scip_install/bin
