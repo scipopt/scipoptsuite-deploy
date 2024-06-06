047d1f3d7c9b7a2fc17394866a7905c3acccccfb
Mark Turner <turner@zib.de>
2024-06-06 12:11:21 +0200

    Add metis 32 bit. Go back to old metis for macos

--- .github/workflows/scripts/macos.bash
+++ .github/workflows/scripts/macos.bash
@@ -29,28 +29,28 @@ with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > $GITHUB_WORKSPA
 #./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
 #make install -j
 
-cd $GITHUB_WORKSPACE
-wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
-tar -xvf v5.1.1-DistDGL-v0.5.tar.gz
-wget https://github.com/KarypisLab/GKlib/archive/refs/tags/METIS-v5.1.1-DistDGL-0.5.tar.gz
-tar -xvf METIS-v5.1.1-DistDGL-0.5.tar.gz
-mkdir $GITHUB_WORKSPACE/metis
-cd GKlib-METIS-v5.1.1-DistDGL-0.5
-make config prefix=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
-make
-make install
-sed -i'' -e 's/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Werror -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/g' GKlibSystem.cmake
-cd ..
-cd METIS-5.1.1-DistDGL-v0.5
-make config prefix=$GITHUB_WORKSPACE/metis/ gklib_path=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
-make
-make install
 #cd $GITHUB_WORKSPACE
+#wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
+#tar -xvf v5.1.1-DistDGL-v0.5.tar.gz
+#wget https://github.com/KarypisLab/GKlib/archive/refs/tags/METIS-v5.1.1-DistDGL-0.5.tar.gz
+#tar -xvf METIS-v5.1.1-DistDGL-0.5.tar.gz
 #mkdir $GITHUB_WORKSPACE/metis
-#cd $GITHUB_WORKSPACE/metis-5.1.0
-#make config shared=0 prefix=$GITHUB_WORKSPACE/metis/
+#cd GKlib-METIS-v5.1.1-DistDGL-0.5
+#make config prefix=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
 #make
 #make install
+#sed -i'' -e 's/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Werror -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/set(GKlib_COPTIONS "${GKlib_COPTIONS} -Wall -pedantic -Wno-unused-function -Wno-unused-but-set-variable -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-label")/g' GKlibSystem.cmake
+#cd ..
+#cd METIS-5.1.1-DistDGL-v0.5
+#make config prefix=$GITHUB_WORKSPACE/metis/ gklib_path=$GITHUB_WORKSPACE/GKlib-METIS-v5.1.1-DistDGL-0.5
+#make
+#make install
+cd $GITHUB_WORKSPACE
+mkdir $GITHUB_WORKSPACE/metis
+cd $GITHUB_WORKSPACE/metis-5.1.0
+make config shared=0 prefix=$GITHUB_WORKSPACE/metis/
+make
+make install
 
 
 cd $GITHUB_WORKSPACE
