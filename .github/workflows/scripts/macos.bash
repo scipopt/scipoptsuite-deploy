afd5eca5d9c8096c04ddbfcdab936236451f566b
Mark Turner <turner@zib.de>
2024-06-06 13:02:34 +0200

    Add metis build to scip_install

--- .github/workflows/scripts/macos.bash
+++ .github/workflows/scripts/macos.bash
@@ -15,8 +15,8 @@ echo "enable_shared=no
 enable_java=no
 enable_sipopt=no
 with_pic=yes
-with_metis_cflags=\"-I${GITHUB_WORKSPACE}/metis/include\"
-with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > $GITHUB_WORKSPACE/scip_install/share/config.site
+with_metis_cflags=\"-I${GITHUB_WORKSPACE}/scip_install/include\"
+with_metis_lflags=\"-L${GITHUB_WORKSPACE}/scip_install/lib -lmetis\"" > $GITHUB_WORKSPACE/scip_install/share/config.site
 
 #wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
 #tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
@@ -48,7 +48,7 @@ with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > $GITHUB_WORKSPA
 cd $GITHUB_WORKSPACE
 mkdir $GITHUB_WORKSPACE/metis
 cd $GITHUB_WORKSPACE/metis-5.1.0
-make config shared=0 prefix=$GITHUB_WORKSPACE/metis/
+make config shared=0 prefix=$GITHUB_WORKSPACE/scip_install/
 make
 make install
 
@@ -57,7 +57,7 @@ cd $GITHUB_WORKSPACE
 git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps
 ./get.Mumps
-./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install --with-metis-cflags="-I${GITHUB_WORKSPACE}/metis/include" --with-metis-lflags="-L${GITHUB_WORKSPACE}/metis/lib -lmetis"
+./configure --enable-shared=no --enable-static=yes --prefix=$GITHUB_WORKSPACE/scip_install --with-metis-cflags="-I${GITHUB_WORKSPACE}/scip_install/include" --with-metis-lflags="-L${GITHUB_WORKSPACE}/scip_install/lib -lmetis"
 make
 make install
 
@@ -68,7 +68,7 @@ unzip $IPOPT_VERSION.zip
 cd Ipopt-releases-$IPOPT_VERSION
 mkdir build
 cd build
-../configure --prefix=$GITHUB_WORKSPACE/scip_install/ --disable-java --enable-shared=no --disable-sipopt --enable-static=yes --with-metis-cflags="-I${GITHUB_WORKSPACE}/metis/include" --with-metis-lflags="-L${GITHUB_WORKSPACE}/metis/lib -lmetis"
+../configure --prefix=$GITHUB_WORKSPACE/scip_install/ --disable-java --enable-shared=no --disable-sipopt --enable-static=yes --with-metis-cflags="-I${GITHUB_WORKSPACE}/scip_install/include" --with-metis-lflags="-L${GITHUB_WORKSPACE}/scip_install/lib -lmetis"
 make -j
 make test
 make install
