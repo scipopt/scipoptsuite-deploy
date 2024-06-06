f3e9fa33e89f0b88465aaaaf5ddcd7af7a7567bc
Mark Turner <turner@zib.de>
2024-06-06 10:10:00 +0200

    Check if metis links for macos

--- .github/workflows/scripts/macos.bash
+++ .github/workflows/scripts/macos.bash
@@ -11,16 +11,24 @@ export MACOSX_DEPLOYMENT_TARGET=11.0
 rm -rf /usr/local/include/boost
 mkdir /usr/local/include/boost
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
-tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
-mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
+cd $GITHUB_WORKSPACE
+echo "enable_shared=no
+enable_java=no
+enable_sipopt=no
+with_pic=yes
+with_metis_cflags=\"-I${GITHUB_WORKSPACE}/metis/include\"
+with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > $GITHUB_WORKSPACE/scip_install/share/config.site
 
-rm -f /usr/local/lib/libgmp*
-wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
-tar xvf gmp-6.3.0.tar.xz
-cd gmp-6.3.0
-./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
-make install -j
+#wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
+#tar --bzip2 -xf $GITHUB_WORKSPACE/boost_1_82_0.tar.bz2
+#mv $GITHUB_WORKSPACE/boost_1_82_0/boost/* /usr/local/include/boost/.
+#
+#rm -f /usr/local/lib/libgmp*
+#wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
+#tar xvf gmp-6.3.0.tar.xz
+#cd gmp-6.3.0
+#./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
+#make install -j
 
 #cd $GITHUB_WORKSPACE
 #wget https://github.com/KarypisLab/METIS/archive/refs/tags/v5.1.1-DistDGL-v0.5.tar.gz
@@ -51,6 +59,7 @@ git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
 cd ThirdParty-Mumps
 ./get.Mumps
 ./configure --enable-shared=no --enable-static=yes --prefix=/$GITHUB_WORKSPACE/scip_install
+cat config.log
 make
 make install
 
@@ -58,14 +67,6 @@ make install
 cd $GITHUB_WORKSPACE
 wget https://github.com/coin-or/Ipopt/archive/refs/tags/releases/$IPOPT_VERSION.zip
 unzip $IPOPT_VERSION.zip
-echo "enable_shared=no
-enable_java=no
-enable_sipopt=no
-with_pic=yes
-with_metis_cflags=\"-I${GITHUB_WORKSPACE}/metis/include\"
-with_metis_lflags=\"-L${GITHUB_WORKSPACE}/metis/lib -lmetis\"" > $GITHUB_WORKSPACE/scip_install/share/config.site
-
-
 cd Ipopt-releases-$IPOPT_VERSION
 mkdir build
 cd build
@@ -75,27 +76,27 @@ make test
 make install
 
 
-cd $GITHUB_WORKSPACE
-wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
-unzip release-$SOPLEX_VERSION.zip
-cd soplex-release-$SOPLEX_VERSION
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
-make -j
-make test
-make install
-
-
-cd $GITHUB_WORKSPACE
-wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
-unzip v$SCIP_VERSION.zip
-cd scip-$SCIP_VERSION
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true -DGMP=true -DGMP_DIR=$GITHUB_WORKSPACE/scip_install
-make -j
-make test
-make install
-cd ../..
-zip -r $GITHUB_WORKSPACE/libscip-macos.zip scip_install/lib scip_install/include scip_install/bin
+#cd $GITHUB_WORKSPACE
+#wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
+#unzip release-$SOPLEX_VERSION.zip
+#cd soplex-release-$SOPLEX_VERSION
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DGMP=true -DPAPILO=false -DGMP_DIR=$GITHUB_WORKSPACE/scip_install -DMPFR=false -DBOOST=true -DBOOST_INCLUDE_DIR=/usr/local/include/boost/
+#make -j
+#make test
+#make install
+#
+#
+#cd $GITHUB_WORKSPACE
+#wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
+#unzip v$SCIP_VERSION.zip
+#cd scip-$SCIP_VERSION
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../../scip_install -DGMP_DIR=../../scip_install -DPAPILO=false -DZIMPL=false -DGMP=true -DREADLINE=false -DIPOPT=true -DIPOPT_DIR=../../scip_install -DBOOST=true -DGMP=true -DGMP_DIR=$GITHUB_WORKSPACE/scip_install
+#make -j
+#make test
+#make install
+#cd ../..
+#zip -r $GITHUB_WORKSPACE/libscip-macos.zip scip_install/lib scip_install/include scip_install/bin
