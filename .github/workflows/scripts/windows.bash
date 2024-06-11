e592946791579b5e13e16dcb7725fdd2ee4e522c
Mark Turner <turner@zib.de>
2024-06-11 16:13:44 +0200

    moves gmp to scip_install for windows

--- .github/workflows/scripts/windows.bash
+++ .github/workflows/scripts/windows.bash
@@ -1,17 +1,18 @@
-pacman -S --noconfirm unzip git mingw-w64-x86_64-cmake cmake mingw-w64-x86_64-zlib zip mingw-w64-x86_64-boost
-# mingw-w64-x86_64-gmp
+pacman -S --noconfirm unzip git mingw-w64-x86_64-cmake cmake mingw-w64-x86_64-zlib zip mingw-w64-x86_64-boost mingw-w64-x86_64-gmp
 
-cd $GITHUB_WORKSPACE
-wget https://github.com/coin-or/Ipopt/releases/download/releases%2F$IPOPT_VERSION/Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
-unzip Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
-mv Ipopt-$IPOPT_VERSION-win64-msvs2019-md/ scip_install
-mv scip_install/lib/ipopt.dll.lib scip_install/lib/ipopt.lib
+#cd $GITHUB_WORKSPACE
+#wget https://github.com/coin-or/Ipopt/releases/download/releases%2F$IPOPT_VERSION/Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
+#unzip Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
+#mv Ipopt-$IPOPT_VERSION-win64-msvs2019-md/ scip_install
+#mv scip_install/lib/ipopt.dll.lib scip_install/lib/ipopt.lib
 
-wget https://github.com/pmmp/DependencyMirror/releases/download/mirror/gmp-6.3.0.tar.xz
-tar xvf gmp-6.3.0.tar.xz
-cd gmp-6.3.0
-./configure --with-pic --disable-shared --enable-cxx --prefix=$GITHUB_WORKSPACE/scip_install
-make install -j
+mkdir scip_install
+mkdir scip_install/lib
+mkdir scip_install/pkgconfig
+mkdir scip_install/include
+mv D:/a/_temp/msys64/mingw/include/gmp.h scip_install/include
+mv D:/a/_temp/msys64/mingw/lib/libgmp.a scip_install/lib/.
+mv D:/a/_temp/msys64/mingw/pkgconfig/gmp.pc scip_install/pkgconfig/.
 
 export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Enterprise/Common7/Tools"
 export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.37.32822/bin/Hostx64/x64"
@@ -21,21 +22,21 @@ wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION
 unzip release-$SOPLEX_VERSION.zip
 cd soplex-release-$SOPLEX_VERSION
 mkdir soplex_build
-cmake -G "Visual Studio 17 2022" -B soplex_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Release -DPAPILO=false -DGMP=true -DBoost=true -DZLIB=false -DCMAKE_GENERATOR_PLATFORM=x64
+cmake -G "Visual Studio 17 2022" -B soplex_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Release -DPAPILO=false -DGMP=true -DGMP_DIR=../scip_install -DBoost=true -DZLIB=false -DCMAKE_GENERATOR_PLATFORM=x64
 cmake --build soplex_build --config Release
 cmake --install soplex_build
 
 
-cd $GITHUB_WORKSPACE
-mkdir $GITHUB_WORKSPACE/scip_build
-wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
-unzip v$SCIP_VERSION.zip
-cd scip-$SCIP_VERSION
-mkdir scip_build
-cmake -G "Visual Studio 17 2022" -B scip_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=none -DSOPLEX_DIR=../scip_install -DPAPILO=false -DZIMPL=false -DZLIB=false -DREADLINE=false -DGMP=true -DIPOPT=true -DIPOPT_DIR=../scip_install -DIPOPT_LIBRARIES=../scip_install/bin -DBoost=true -DCMAKE_GENERATOR_PLATFORM=x64
-cmake --build scip_build --config Release
-cmake --install scip_build
-ctest
-
-cd $GITHUB_WORKSPACE
-zip -r $GITHUB_WORKSPACE/libscip-windows.zip scip_install/lib scip_install/include scip_install/bin
+#cd $GITHUB_WORKSPACE
+#mkdir $GITHUB_WORKSPACE/scip_build
+#wget https://github.com/scipopt/scip/archive/refs/tags/v$SCIP_VERSION.zip
+#unzip v$SCIP_VERSION.zip
+#cd scip-$SCIP_VERSION
+#mkdir scip_build
+#cmake -G "Visual Studio 17 2022" -B scip_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Release -DLPS=spx -DSYM=none -DSOPLEX_DIR=../scip_install -DPAPILO=false -DZIMPL=false -DZLIB=false -DREADLINE=false -DGMP=true -DIPOPT=true -DIPOPT_DIR=../scip_install -DIPOPT_LIBRARIES=../scip_install/bin -DBoost=true -DCMAKE_GENERATOR_PLATFORM=x64
+#cmake --build scip_build --config Release
+#cmake --install scip_build
+#ctest
+#
+#cd $GITHUB_WORKSPACE
+#zip -r $GITHUB_WORKSPACE/libscip-windows.zip scip_install/lib scip_install/include scip_install/bin
