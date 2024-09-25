c8c7165ed07b5690bb1c2bbe6a9e93c0c9f05362
Mark Turner <turner@zib.de>
2024-09-25 15:20:19 +0200

    Remove IPOPT from windows build

--- .github/workflows/scripts/windows.bash
+++ .github/workflows/scripts/windows.bash
@@ -1,10 +1,4 @@
 pacman -S --noconfirm unzip git mingw-w64-x86_64-cmake cmake mingw-w64-x86_64-zlib zip mingw-w64-x86_64-boost
-            
-cd $GITHUB_WORKSPACE
-wget https://github.com/coin-or/Ipopt/releases/download/releases%2F$IPOPT_VERSION/Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
-unzip Ipopt-$IPOPT_VERSION-win64-msvs2019-md.zip
-mv Ipopt-$IPOPT_VERSION-win64-msvs2019-md/ scip_install
-mv scip_install/lib/ipopt.dll.lib scip_install/lib/ipopt.lib
 
 cd $GITHUB_WORKSPACE
 wget https://github.com/scipopt/soplex/archive/refs/tags/release-$SOPLEX_VERSION.zip
@@ -28,7 +22,7 @@ mkdir scip_build
 export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Enterprise/Common7/Tools"
 export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.37.32822/bin/Hostx64/x64"
 export PATH="$PATH:/c/Program Files/Microsoft Visual Studio/2022/Enterprise/MSBuild/Current/Bin"
-cmake -G "Visual Studio 17 2022" -B scip_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Debug -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../scip_install -DPAPILO=false -DZIMPL=false -DZLIB=false -DREADLINE=false -DGMP=false -DBoost=true -DIPOPT=true -DIPOPT_DIR=../scip_install -DIPOPT_LIBRARIES=../scip_install/bin -DTPI=tny -DCMAKE_GENERATOR_PLATFORM=x64
+cmake -G "Visual Studio 17 2022" -B scip_build -DCMAKE_INSTALL_PREFIX=../scip_install -DCMAKE_BUILD_TYPE=Debug -DLPS=spx -DSYM=snauty -DSOPLEX_DIR=../scip_install -DPAPILO=false -DZIMPL=false -DZLIB=false -DREADLINE=false -DGMP=false -DBoost=true -DIPOPT=false -DTPI=tny -DCMAKE_GENERATOR_PLATFORM=x64
 cmake --build scip_build --config Debug
 cmake --install scip_build --config Debug
 ctest
