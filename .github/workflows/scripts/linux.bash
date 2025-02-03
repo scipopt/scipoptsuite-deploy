0755c012be48a840ea70781dbd8cff9a0c834b4d
Mark Turner <turner@zib.de>
2025-02-03 11:51:33 +0100

    Remove cmake directory in lib64

--- .github/workflows/scripts/linux.bash
+++ .github/workflows/scripts/linux.bash
@@ -9,7 +9,7 @@ rm -rf /usr/include/boost
 mkdir /usr/include/boost
 
 wget https://archives.boost.io/release/1.82.0/source/boost_1_82_0.tar.gz
-tar -xzf $GITHUB_WORKSPACE/boost_1_82_0.tar.gz
+tar -xvf $GITHUB_WORKSPACE/boost_1_82_0.tar.gz
 cd boost_1_82_0
 ./bootstrap.sh --prefix=/usr/include/boost --with-libraries=program_options,serialization,iostreams,regex
 ./b2 --prefix=/usr/include/boost -j8 install
@@ -113,5 +113,6 @@ make install
 
 cd $GITHUB_WORKSPACE
 mkdir -p scip_install/lib
+rm -rf scip_install/lib64/cmake
 mv scip_install/lib64/* scip_install/lib/.
 zip -r $GITHUB_WORKSPACE/libscip-linux.zip scip_install/lib scip_install/include scip_install/bin
